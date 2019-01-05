98952bee8ff026e758085ebcadf748a90484e412
bdk <whoasbdk@gmail.com>
2019-01-05 15:10:18 +0800

    完成学生班级的关联查询

--- routes/classes.js
+++ routes/classes.js
@@ -24,7 +24,7 @@ router.get("/:id",async function(req,res){
 //增加班级
 router.post("/",async function(req,res){
     let {name,date} = req.body;
-    let data = await client.post("/classes",{name,date});
+    let data = await client.post("/classes",{name,date,count:0});
     res.send(data);
 });
 
@@ -43,6 +43,16 @@ router.delete("/:id",async function(req,res){
     res.send(data);
 });
 
-
+//改变班级人数
+const changeClassesCount = async function(id,increment){
+    //根据ID查询班级
+    let classesData = await client.get("/classes/"+id);   
+    //根据传递的参数对该班级的人数进行增(正数)或减(负数)
+    classesData.count = parseInt(classesData.count || 0) + increment;
+    //传递给后台进行修改的数据中不需要id
+    delete classesData._id;
+    await client.put("/classes/"+id,classesData);
+}
 
 module.exports = router;
+module.exports.changeClassesCount = changeClassesCount;
--- routes/students.js
+++ routes/students.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const {changeClassesCount} = require("./classes");
 const db = require("ykt-mongo");
 const client = require("ykt-http-client");
 client.url("localhost:8080");
@@ -14,7 +15,7 @@ router.get("/",async function(req,res){
     // db.collection("students").findByPage(page,rows,option,function(data){
     //     res.send(data);
     // });
-    let data = await client.get("/students",{page,rows,...option});
+    let data = await client.get("/students",{page,rows,submitType:"findJoin",ref:"classes",...option});
     res.send(data);
 });
 
@@ -30,22 +31,31 @@ router.get("/:id",async function(req,res){
 
 //增加学生
 router.post("/",async function(req,res){
-    let {name,age,gender,headImg} = req.body;
+    let {name,age,gender,headImg,classesId} = req.body;
     // db.collection("students").insert({name,age,gender,headImg},function(data){
     //     res.send(data);
     // });
-    let data = await client.post("/students",{name,age,gender,headImg});
+    let data = await client.post("/students",{name,age,gender,headImg,classes:{$id:classesId,$ref:"classes"}});
+    await changeClassesCount(classesId,1);
     res.send(data);
 });
 
 //修改学生
 router.put("/:id",async function(req,res){
-    let {name,age,gender} = req.body;
+    let {name,age,gender,classesId} = req.body;
+    console.log( req.body);
     let id = req.params.id;
     // db.collection("students").update({_id:db.ObjectID(id)},{name,age,gender},function(data){
     //     res.send(data);
     // });
-    let data = await client.put("/students/"+id,{name,age,gender});
+    let studentData = await client.get("/students/"+id);
+    let data = await client.put("/students/"+id,{name,age,gender,classes:{$ref:"classes",$id:classesId}});
+    if(studentData.classes){
+        console.log("studentData.classes",studentData.classes);
+        await changeClassesCount(studentData.classes.$id,-1);
+    }
+    await changeClassesCount(classesId,1);
+    
     res.send(data);
 });
 
@@ -58,7 +68,6 @@ router.delete("/:id",async function(req,res){
     let data = await client.delete("/students/"+id);
     res.send(data);
 });
-
 
 
 module.exports = router;
