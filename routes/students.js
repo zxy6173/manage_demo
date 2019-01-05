1c1b808832605c4e5c78f73f73a265bd9523ddc5
bdk <whoasbdk@gmail.com>
2019-01-05 13:14:49 +0800

    实现前后端分离

--- routes/students.js
+++ routes/students.js
@@ -1,50 +1,62 @@
 var express = require('express');
 var router = express.Router();
 const db = require("ykt-mongo");
+const client = require("ykt-http-client");
+client.url("localhost:8080");
 
 //查询所有
-router.get("/",function(req,res){
+router.get("/",async function(req,res){
     let {page,rows,type,value} = req.query;
     let option = {};
     if(type && value){
-        option = {[type]:{$regex:value}}
+        option = {[type]:value}
     }
-    db.collection("students").findByPage(page,rows,option,function(data){
-        res.send(data);
-    });
+    // db.collection("students").findByPage(page,rows,option,function(data){
+    //     res.send(data);
+    // });
+    let data = await client.get("/students",{page,rows,...option});
+    res.send(data);
 });
 
 //根据ID查询学生
-router.get("/:id",function(req,res){
+router.get("/:id",async function(req,res){
     let id = req.params.id;
-    db.collection("students").find({_id:db.ObjectID(id)},function(data){
-        res.send(data[0]);
-    });
+    // db.collection("students").find({_id:db.ObjectID(id)},function(data){
+    //     res.send(data[0]);
+    // });
+    let data = await client.get("/students/"+id);
+    res.send(data);
 });
 
 //增加学生
-router.post("/",function(req,res){
+router.post("/",async function(req,res){
     let {name,age,gender,headImg} = req.body;
-    db.collection("students").insert({name,age,gender,headImg},function(data){
-        res.send(data);
-    });
+    // db.collection("students").insert({name,age,gender,headImg},function(data){
+    //     res.send(data);
+    // });
+    let data = await client.post("/students",{name,age,gender,headImg});
+    res.send(data);
 });
 
 //修改学生
-router.put("/:id",function(req,res){
+router.put("/:id",async function(req,res){
     let {name,age,gender} = req.body;
     let id = req.params.id;
-    db.collection("students").update({_id:db.ObjectID(id)},{name,age,gender},function(data){
-        res.send(data);
-    });
+    // db.collection("students").update({_id:db.ObjectID(id)},{name,age,gender},function(data){
+    //     res.send(data);
+    // });
+    let data = await client.put("/students/"+id,{name,age,gender});
+    res.send(data);
 });
 
 //删除学生
-router.delete("/:id",function(req,res){
+router.delete("/:id",async function(req,res){
     let id = req.params.id;
-    db.collection("students").remove({_id:db.ObjectID(id)},function(data){
-        res.send(data);
-    });
+    // db.collection("students").remove({_id:db.ObjectID(id)},function(data){
+    //     res.send(data);
+    // });
+    let data = await client.delete("/students/"+id);
+    res.send(data);
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,70 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const db = require("ykt-mongo");
-
+const client = require("ykt-http-client");
+client.url("localhost:8080");
 //验证手机号是否存在
-router.get("/",function(req,res){
+router.get("/", async function(req, res) {
     let phone = req.query.phone;
-    db.collection("users").find({phone},function(data){
-        if(data.length > 0){
-            res.send({
-                status:0
-            });
-        }else{
-            res.send({
-                status:1
-            });
-        }
-    });
-})
-
+    // db.collection("users").find({phone},function(data){
+    //     if(data.length > 0){
+    //         res.send({
+    //             status:0
+    //         });
+    //     }else{
+    //         res.send({
+    //             status:1
+    //         });
+    //     }
+    // });
+    let data = await client.get("/users", { phone });
+    if (data.length > 0) {
+        res.send({
+            status: 0
+        });
+    } else {
+        res.send({
+            status: 1
+        });
+    }
+});
 
 //登录
-router.post("/login",function(req,res){
-    let {phone,pwd} = req.body;
-    db.collection("users").find({phone,pwd},function(data){
-        if(data.length > 0){
-            req.session.user = data[0];
-            res.send({
-                status:1
-            });
-        }else{
-            res.send({
-                status:0
-            });
-        }
-    });
+router.post("/login", async function(req, res) {
+    let { phone, pwd } = req.body;
+    // db.collection("users").find({ phone, pwd }, function(data) {
+    //     if (data.length > 0) {
+    //         req.session.user = data[0];
+    //         res.send({
+    //             status: 1
+    //         });
+    //     } else {
+    //         res.send({
+    //             status: 0
+    //         });
+    //     }
+    // });
+    let data = await client.post("/login", { phone, pwd });
+    if (data.phone) {
+        req.session.user = data;
+        res.send({
+            status: 1
+        });
+    } else {
+        res.send({
+            status: 0
+        });
+    }
 });
 
 //注册
-router.post("/",function(req,res){
-    let {phone,pwd} = req.body;
-    db.collection("users").insert({phone,pwd},function(data){
-        res.send(data);
-    });
+router.post("/", function(req, res) {
+    let { phone, pwd } = req.body;
+    // db.collection("users").insert({ phone, pwd }, function(data) {
+    //     res.send(data);
+    // });
+    let data = client.post("/users",{phone,pwd});
+    res.send(data);
 });
 
 module.exports = router;
